9cd3bab747caf5c8bb689773e0dba114e5467530
Abdullah Jamil <abdullahjamil819@gmail.com>
2023-12-09 01:19:30 +0500

    React http request practice

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMovesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      //default method of fetch is GET
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMovesHandler();
+  }, [fetchMovesHandler]);
+
+  // instead of conditional rendering in the return statement, we can also use a variable i-e: "let content" and assign it values by checking loading or error etc using IF and just render "content" variable in the end
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>No Movies Found.</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
